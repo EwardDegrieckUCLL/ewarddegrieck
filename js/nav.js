3e6ed30762f53dcd2030146240a88e99c76c6fc3
Eward Degrieck UCLL <eward.degrieck@student.ucll.be>
2025-08-10 08:20:16 +0200

    disabled scrolling when opening hamburger menu

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,7 @@
 /* variables */
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navList = document.querySelector('nav ul');
+const body = document.querySelector('body');
 
 /* functions */
 
@@ -8,4 +9,5 @@ const navList = document.querySelector('nav ul');
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('active');
     navList.classList.toggle('active');
+    body.classList.toggle('disable-scrolling');
 });
